96cec72d79f253ea5489d9f1747852b410b36b2b
Madhu <Madhu@192.168.100.102>
2021-01-04 14:57:51 +0530

    Packages

---
+++ BECA8A9/src/com/jspiders/pkg1/Sample.java
@@ -0,0 +1,26 @@
+package com.jspiders.pkg1;
+
+public class Sample 
+{
+  public static void test()
+  {
+	  System.out.println("this is static-test() of Sample");
+  }
+  
+  public void display()
+  {
+	  System.out.println("this is display() of Sample");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BECA8A9/src/com/jspiders/pkg2/Demo.java
@@ -0,0 +1,13 @@
+package com.jspiders.pkg2;
+
+import com.jspiders.pkg1.Sample;
+
+public class Demo 
+{
+	public static void main(String[] args) 
+	{
+	   Sample.test();
+	   
+	   new Sample().display();
+	}
+}
