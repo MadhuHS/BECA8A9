5617ee0e0f92bac645825f95dc5d10aee1108bfa
Madhu <Madhu@192.168.0.103>
2021-01-23 15:28:01 +0530

    DS

--- BECA8A9/src/ds/Mainclass.java
+++ BECA8A9/src/ds/Mainclass.java
@@ -2,38 +2,50 @@ package ds;
 
 public class Mainclass 
 {
-	public static void printTeams(String[] teams)
+	public static void printTeams(String[] teams) 
 	{
-		for(int i=0;i<teams.length;i++)
+		for (int i = 0; i < teams.length; i++) 
 		{
-			if(teams[i]!=null)
-	     	System.out.println(teams[i]);
+			if (teams[i] != null)
+				System.out.println(teams[i]);
 		}
 	}
-	
-	public static void printTeamName(int index)
+
+	public static void printTeamName(String[] teams, int index) 
+	{
+		if(index < teams.length)
+		{
+		 System.out.println(teams[index]);
+		}
+		else
+		{
+		  System.out.println("N/A");
+		}
+	}
+	public static boolean searchTeam(String[] teams,String team)
 	{
 		
 	}
-	public static void main(String[] args) 
-	{
-		 String[] teams1 = new String[10];
-	        teams1[0] = "RCB";
-	        teams1[1] = "CSK";
-	        teams1[2] = "MI";
-	        
-        String[] teams2 = new String[10];
-        teams2[0] = "RCB";
-        teams2[1] = "CSK";
-        teams2[2] = "MI";
-        teams2[3] = "KKR";
-        teams2[4] = "SRH";
-        teams2[5] = "DC";
-        teams2[6] = "RR";
-        teams2[7] = "KXI";
-        
-        printTeams(teams2);
-        printTeams(teams1);
-        
-        	}
+
+
+	public static void main(String[] args) {
+		String[] teams1 = new String[10];
+		teams1[0] = "RCB";
+		teams1[1] = "CSK";
+		teams1[2] = "MI";
+
+		String[] teams2 = new String[10];
+		teams2[0] = "RCB";
+		teams2[1] = "CSK";
+		teams2[2] = "MI";
+		teams2[3] = "KKR";
+		teams2[4] = "SRH";
+		teams2[5] = "DC";
+		teams2[6] = "RR";
+		teams2[7] = "KXI";
+
+		
+		printTeamName(teams1,20);
+
+	}
 }
