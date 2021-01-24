1fe48a13a60e54c9383c2d7c09a6017ea7c63808
Madhu <Madhu@192.168.0.103>
2021-01-24 09:50:22 +0530

    DS

--- BECA8A9/src/ds/Mainclass2.java
+++ BECA8A9/src/ds/Mainclass2.java
@@ -2,31 +2,49 @@ package ds;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-public class Mainclass2 
-{
-	public static void printTeams(String[] teams) 
-	{
-		Stream<String> stm = Arrays.stream(teams);//create a stream and return addrs
-	    
+public class Mainclass2 {
+	public static void printTeams(String[] teams) {
+		Stream<String> stm = Arrays.stream(teams);// create a stream and return addrs
+
 //Consumer<String> ref = (String str)-> System.out.println(str);
-		//stm.forEach((String str)-> System.out.println(str));
-		stm.forEach( str -> System.out.println(str));
-	    
-		Arrays.stream(teams)
-		      .forEach(str2 -> System.out.println(str2));
-		
-		//.forEach(varName -> stmt);
+		// stm.forEach((String str)-> System.out.println(str));
+		stm.forEach(str -> System.out.println(str));
+
+		Arrays.stream(teams).forEach(str2 -> System.out.println(str2));
+
+		// .forEach(varName -> stmt);
 	}
 
 	public static void printTeamName(String[] teams, int index) 
 	{
-		
+		if(index < teams.length)
+		{
+		 System.out.println(teams[index]);
+		}
+		else
+		{
+		  System.out.println("N/A");
+		}
 	}
-	public static boolean searchTeam(String[] teams,String team)
+
+	public static boolean searchTeam(String[] teams, String team) 
 	{
-		return false;
+		
+         boolean b1 = Arrays.stream(teams)//create a stream
+              .filter(str -> str!=null)//create a new stream without null value
+              .findAny()//find one element
+              .get()
+              .equalsIgnoreCase(team);//compare element with gvn team
+         
+        return b1;
+        
+	}
+	public static void searchTeam(String[] teams)
+	{
+	  //print only team names with length more than 3 chars	
 	}
 
 	public static void main(String[] args) {
@@ -45,6 +63,6 @@ public class Mainclass2
 		teams2[6] = "RR";
 		teams2[7] = "KXI";
 
-		printTeams(teams1);
+		System.out.println(searchTeam(teams2,"AAA"));
 	}
 }
